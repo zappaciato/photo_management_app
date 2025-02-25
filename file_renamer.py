7d17ba104776636e2c9563576ab86303ca49fb53
Krzysztof Flakiewicz <kflakiewicz@evoila.com>
2025-02-25 15:56:58 +0100

    base refactoring

--- file_renamer.py
+++ file_renamer.py
@@ -1,37 +1,29 @@
 import os
-from metadata_extractor import get_image_metadata
+from metadata_extractor import get_image_metadata, format_metadata
 from unique_filename_generator import generate_unique_suffix
 
-def rename_files(folder_path, output_path):
-    print('works file_renamer')
+def rename(new_filename_base, unique_suffix, output_path, image_path, filename):
+    new_filename = f"{new_filename_base}.{unique_suffix}.jpg"
+    new_image_path = os.path.join(output_path, new_filename)
+    # Rename the file
+    os.rename(image_path, new_image_path)
+    print(f"Renamed {filename} to {new_filename}")
 
-    file_counts = {}  # Dictionary to store file counts for serial numbers
+def rename_files(folder_path, output_path):
     for root, _, filenames in os.walk(folder_path):
         for filename in filenames:
             if filename.lower().endswith(('.jpg', '.jpeg', '.png')):
                 image_path = os.path.join(root, filename)
-                # lat, lon, photo_datetime, image_url = get_image_metadata(image_path)
                 lat, lon, photo_datetime = get_image_metadata(image_path)
                 
                 if lat is not None and lon is not None and photo_datetime is not None:
-                    lat_str = str(int(lat * 10000)).replace('.', '')
-                    lon_str = str(int(lon * 10000)).replace('.', '')
-                    date_str = photo_datetime.strftime("%Y-%m")
-                    new_filename_base = f"{lat_str}.{lon_str}.{date_str}"
+                    print("about to cread new filename base")
 
-                    if new_filename_base in file_counts:
-                        file_counts[new_filename_base] += 1
-                    else:
-                        file_counts[new_filename_base] = 1
-
-                    # serial_number = str(file_counts[new_filename_base]).zfill(5)
+                    new_filename_base = format_metadata(lat, lon, photo_datetime)
                     unique_suffix = generate_unique_suffix()
-                    new_filename = f"{new_filename_base}.{unique_suffix}.jpg"
-                    new_image_path = os.path.join(output_path, new_filename)
-                    # Rename the file
-                    os.rename(image_path, new_image_path)
-                    print(f"Renamed {filename} to {new_filename}")
-                else:
+                    rename(new_filename_base, unique_suffix, output_path, image_path, filename)
+
+                else:       
                     print(f"Metadata missing for {filename}, skipping.")
-                    lat, lon, photo_datetime, image_url = get_image_metadata(image_path)
+                    # lat, lon, photo_datetime = get_image_metadata(image_path)
     
--- thumbnails_generator.py
+++ thumbnails_generator.py
@@ -1,8 +1,26 @@
 from PIL import Image
+import os
+
+def create_thumbnail_folder(output_path):
+    image_dir = os.path.join(output_path, "thumbnails")  # Create a thumbnails directory
+    os.makedirs(image_dir, exist_ok=True)
+    return image_dir
+
+def create_thumbnail_file(image_dir, filename, image_path):
+    """Creates a thumbnail file and returns its path."""
+    try:
+        thumb_filename = f"{os.path.splitext(filename)[0]}.thumb.jpg"
+        thumb_path = os.path.join(image_dir, thumb_filename)
+        create_thumbnail(image_path, thumb_path)
+        return thumb_filename
+    except Exception as e:
+        print(f"Error creating thumbnail for {filename}: {e}")
+        return None  # Return None if thumbnail creation fails
 
 def create_thumbnail(image_path, thumb_path, size=(400, 300)):
     """Creates a thumbnail image file."""
-
+    # print(f"Thumbnail image_path: {image_path}") #Debug print
+    # print(f"Thumbnail thumb_path: {thumb_path}") #Debug print
     try:
         img = Image.open(image_path)
         img.thumbnail(size)
